0e681c7682a08115ed12cc690a13fb95fd4afd95
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-04-23 09:09:44 +0200

    Improve

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 3;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11", "fontawesome:size=11" };
+static const char *fonts[]          = { "monospace:size=11", "fontawesome:size=13" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
