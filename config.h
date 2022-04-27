cc4114b449207940bae3062f2de268ae86030008
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-04-28 00:25:13 +0200

    Fix undeclared function error 😆

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "layouts.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
