71fa0124992c9052047ea95285a552419411ad68
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-05-19 18:14:51 +0200

    Suport colred emoji 😄

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11", "fontawesome:size=16" };
+static const char *fonts[]          = { "monospace:size=11", "Noto color emoji:size=13" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
