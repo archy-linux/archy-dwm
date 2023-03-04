639c18a09553a96ec2ed4593aeb51dd5332e9b76
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-03-04 12:12:51 +0200

    Return the mod key to the super key agine :)

--- src/keys/keys.h
+++ src/keys/keys.h
@@ -1,27 +1,31 @@
 #ifndef KEYS_H
 #define KEYS_H
 
-#include <X11/X.h>
 #include "../dwm.h"
+#include <X11/X.h>
 
 typedef struct {
-    unsigned int mod;
-    KeySym chain;
-    KeySym keysym;
+  unsigned int mod;
+  KeySym chain;
+  KeySym keysym;
 
-    void (*func)(const Arg *);
+  void (*func)(const Arg *);
 
-    const Arg arg;
+  const Arg arg;
 } Key;
 
 /* key definitions */
-#define MODKEY LockMask // The modifier key is: Caps Lock :P
+#define MODKEY Mod4Mask // Super key
 
-#define TAGKEYS(CHAIN, KEY, TAG) \
-   { MODKEY,                       CHAIN,    KEY,      view,           {.ui = 1 << TAG} }, \
-   { MODKEY|ControlMask,           CHAIN,    KEY,      toggleview,     {.ui = 1 << TAG} }, \
-   { MODKEY|ShiftMask,             CHAIN,    KEY,      tag,            {.ui = 1 << TAG} }, \
-   { MODKEY|ControlMask|ShiftMask, CHAIN,    KEY,      toggletag,      {.ui = 1 << TAG} },
+#define TAGKEYS(CHAIN, KEY, TAG)                                               \
+  {MODKEY, CHAIN, KEY, view, {.ui = 1 << TAG}},                                \
+      {MODKEY | ControlMask, CHAIN, KEY, toggleview, {.ui = 1 << TAG}},        \
+      {MODKEY | ShiftMask, CHAIN, KEY, tag, {.ui = 1 << TAG}},                 \
+      {MODKEY | ControlMask | ShiftMask,                                       \
+       CHAIN,                                                                  \
+       KEY,                                                                    \
+       toggletag,                                                              \
+       {.ui = 1 << TAG}},
 
 // Include all the key bindings
 #ifndef KEYS_C
