43871cc01ee344a5850ec928dcfc55ad0e1b402f
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-11-20 01:11:32 +0200

    feat(colorscheme): merge the gruvbox theme with the config

--- src/config/config.h
+++ src/config/config.h
@@ -28,14 +28,25 @@ static const char *fonts[] = {
     "Nerd Font Mono:size=10:antialias=true:hinting=true"};
 static const char dmenufont[] = "monospace:size=8";
 
-#ifndef STDDEF_H
+/* Colors - Gruvbox dark hard */
+static const char norm_fg[] = "#D5C4A1"; // light2
+static const char norm_bg[] = "#1D2021"; // dark0_hard
+static const char norm_border[] = "#504945"; // dark2
 
-#include <stddef.h>
+static const char sel_fg[] = "#D79921"; // neutral_yellow
+static const char sel_bg[] = "#1D2021"; // dark0_hard
+static const char sel_border[] = "#7C6F64"; // dark4
 
-#endif
+static const char urg_fg[] = "#D5C4A1"; // light4
+static const char urg_bg[] = "#FB4934"; // bright_red
+static const char urg_border[] = "#458588"; // neutral_blue
 
-#include "colors/gruvbox_dark_hard.h"
-#include "archy_dwm.h"
+static const char *colors[][3] = {
+        /*               fg           bg         border                         */
+        [SchemeNorm] = {norm_fg, norm_bg, norm_border}, // unfocused wins
+        [SchemeSel]  = {sel_fg, sel_bg, sel_border},  // the focused win
+        [SchemeUrg] =  {urg_fg, urg_bg, urg_border},
+};
 
 // Autostart programs
 static const char *const autostart[] = {
