6bfa127871a7dfea8ad3051c80d88fd8846827ce
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-11-20 01:14:03 +0200

    feat: incress the factor of master area to 60%

--- src/config/config.h
+++ src/config/config.h
@@ -92,7 +92,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.55f; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.60f; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;     /* number of clients in master area */
 static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
